39410b8c3a2800d051e8ee5025fe17c8823725a5
raviMukti <shinjiravi@gmail.com>
2019-11-14 13:49:41 +0700

    Initialize

---
+++ src/test/java/com/ravi/mukti/WebserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ravi.mukti;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
